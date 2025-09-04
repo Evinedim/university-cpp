21339e0e4e8f9fdd49017c381393528a376fe3c1
Evinedim <nikvoron3008@gmail.com>
2025-09-04 13:26:09 +0300

    second commit

--- vorontsov-ac4-lab-0.cpp
+++ vorontsov-ac4-lab-0.cpp
@@ -1,9 +1,16 @@
 #include <iostream>
-
 using namespace std;
+
+void f()
+{
+    int x;
+}
 
 int main()
 {
+    int x;
+    cin >> x;
+    f();
     cout << "Hello World!" << endl;
     return 0;
 }
